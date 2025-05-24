414654f8c8046b8d910e15529145725f770cf4f1
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-05-25 08:49:06 +0900

    Update voidsetup.sh

--- void/voidsetup.sh
+++ void/voidsetup.sh
@@ -3,15 +3,16 @@
 sudo xbps-install -Syu
 
 sudo xbps-install -S xf86-video-intel mesa-dri
-sudo xbps-install -S NetworkManager
+sudo xbps-install -S NetworkManager network-manager-applet
 sudo ln -sfv /etc/sv/NetworkManager /var/service
-sudo xbps-install -S pipewire wireplumber pipewire-pulse
-sudo ln -s /etc/sv/pipewire /var/service
-sudo ln -s /etc/sv/wireplumber /var/service
+sudo xbps-install -S pipewire wireplumber pipewire-pulse pavucontrol
+sudo ln -sfv /etc/sv/pipewire /var/service
+sudo ln -sfv /etc/sv/wireplumber /var/service
 sudo xbps-install -S noto-fonts-cjk noto-fonts-emoji
 sudo xbps-install -S fcitx5 fcitx5-mozc fcitx5-configtool
+sudo xbps-install -S bluez blueman
 
-sudo xbps-install -S vim neovim htop ufw gufw gvfs xdg-user-dirs-gtk
+sudo xbps-install -S vim neovim htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow
 sudo xbps-install -S foot mako wl-clipboard
 
 # Sway
