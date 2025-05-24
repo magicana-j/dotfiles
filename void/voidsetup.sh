3ab79e9e4c225fa524d25bb3982657ea5bc808dd
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-05-25 07:46:20 +0900

    Create voidsetup.sh

---
+++ void/voidsetup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo xbps-install -Syu
+sudo xbps-install -y sway swaybg swayidle swaylock wofi foot alacritty mako grim slurp light dmidecode imv mpv zathura zathura-pdf-data pulseaudio pulseaudio-utils pavucontrol pipewire pipewire-pulse wireplumber bluez bluez-libs blueman font-fira-code font-hack-nerd font-noto-cjk font-noto-emoji firefox google-chrome fcitx5 fcitx5-anthy fcitx5-gtk fcitx5-qt xdg-utils mesa-dri dbus elogind polkit-elogind seatd openssh xclip grim slurp light dunst lxappearance file-roller unrar unzip p7zip tar xz gzip bzip2 git htop neofetch neovim nano bash-completion curl wget rsync networkmanager NetworkManager-applet usbutils pciutils lshw mesa-dri vulkan-loader intel-media-driver # 必要に応じてnvidia-driversなども
