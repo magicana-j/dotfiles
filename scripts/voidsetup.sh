98ebd82fcbea95c0ca300d6c253d020603328a6e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-05-25 14:04:06 +0900

    Update voidsetup.sh

--- scripts/voidsetup.sh
+++ scripts/voidsetup.sh
@@ -2,18 +2,21 @@
 
 sudo xbps-install -Syu
 
+sudo xbps-install -S dbus elogind
+sudo ln -snfv etc/sv/dbus /var/service/
+sudo ln -snfv etc/sv/elogind /var/service/
 sudo xbps-install -S xf86-video-intel mesa-dri openh264 ffmpeg
 sudo xbps-install -S NetworkManager network-manager-applet
-sudo ln -sfv /etc/sv/NetworkManager /var/service
+sudo ln -snfv /etc/sv/NetworkManager /var/service/
 sudo xbps-install -S pipewire wireplumber pipewire-pulse pavucontrol
-sudo ln -sfv /etc/sv/pipewire /var/service
-sudo ln -sfv /etc/sv/wireplumber /var/service
-sudo xbps-install -S noto-fonts-cjk noto-fonts-emoji
-sudo xbps-install -S fcitx5 fcitx5-mozc fcitx5-configtool
+sudo ln -snfv /etc/sv/pipewire /var/service/
+sudo ln -snfv /etc/sv/wireplumber /var/service/
+sudo xbps-install -S noto-fonts-cjk noto-fonts-emoji font-adobe-source-code-pro
+sudo xbps-install -S fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt fcitx5-qt6
 sudo xbps-install -S bluez blueman
 
-sudo xbps-install -S vim neovim htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow
-sudo xbps-install -S foot mako wl-clipboard
+sudo xbps-install -S vim neovim htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow firefox
+sudo xbps-install -S foot mako wl-clipboard dolphin
 
 # Sway
 sudo xbps-install -S sway swaylock swayidle waybar wofi grim slurp xdg-utils xdg-desktop-portal-wlr
