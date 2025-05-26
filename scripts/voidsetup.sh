464f39c880246b1d34dcede5d8eb72de71ad7ed4
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-05-26 18:53:41 +0900

    Update voidsetup.sh

--- scripts/voidsetup.sh
+++ scripts/voidsetup.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
-sudo echo 'repository=https://repo-fastly.voidlinux.org/current/nonfree' > /etc/xbps.d/10-repository-nonfree.conf
 sudo xbps-install -Syu
+sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 
-sudo zbpa-install -S xorg wayland xorg-server-xwayland
-sudo xbps-install -S dbus elogind
+sudo xbps-install -Sy vim neovim htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow
+sudo zbpa-install -Sy wayland xorg-server-xwayland
+sudo xbps-install -Sy dbus elogind polkit
+sudo xbps-install -Sy xf86-video-intel mesa-dri intel-ucode
+sudo xbps-install -Sy openh264 ffmpeg
+sudo xbps-install -Sy NetworkManager network-manager-applet
+sudo xbps-install -Sy pipewire wireplumber pipewire-pulse pavucontrol
+sudo xbps-install -Sy noto-fonts-cjk noto-fonts-emoji font-adobe-source-code-pro
+sudo xbps-install -Sy fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt5 fcitx5-qt6
+sudo xbps-install -Sy bluez blueman
+
 sudo ln -snfv /etc/sv/dbus /var/service/
 sudo ln -snfv /etc/sv/elogind /var/service/
-sudo xbps-install -S xf86-video-intel mesa-dri intel-ucode
-sudo xbps-install -S openh264 ffmpeg
-sudo xbps-install -S NetworkManager network-manager-applet
 sudo ln -snfv /etc/sv/NetworkManager /var/service/
-sudo xbps-install -S pipewire wireplumber pipewire-pulse pavucontrol
 sudo ln -snfv /etc/sv/pipewire /var/service/
 sudo ln -snfv /etc/sv/wireplumber /var/service/
-sudo xbps-install -S noto-fonts-cjk noto-fonts-emoji font-adobe-source-code-pro
-sudo xbps-install -S fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-gtk+3 fcitx5-gtk4 fcitx5-qt fcitx5-qt6
-sudo xbps-install -S bluez blueman
 
-sudo xbps-install -S vim neovim htop fastfetch ufw gufw gvfs xdg-user-dirs-gtk stow firefox
-sudo xbps-install -S foot mako wl-clipboard dolphin
-sudo xbps-install -S Thunar thunar-archive-plugin zip unzip p7zip
+sudo xbps-install -Sy firefox
+sudo xbps-install -Sy foot kitty alacritty mako wl-clipboard dolphin
+sudo xbps-install -Sy Thunar thunar-archive-plugin zip unzip p7zip
 
 # Sway
-sudo xbps-install -S sway swaylock swayidle Waybar wofi grim slurp xdg-utils xdg-desktop-portal-wlr
+sudo xbps-install -Sy sway swaylock swayidle Waybar wofi grim slurp xdg-utils xdg-desktop-portal-wlr
 
+echo 'Completed.'
